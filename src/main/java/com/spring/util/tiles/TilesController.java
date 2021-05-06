053c8645f5501088f829ce1663ae0d5bba8a48d4
YSH-oooee <rhbhoi5@gmail.com>
2021-05-06 14:49:43 +0900

    2105061215_tiles(put-attribute, content 삽입)

---
+++ src/main/java/com/spring/util/tiles/TilesController.java
@@ -0,0 +1,25 @@
+package com.spring.util.tiles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("tiles")
+public class TilesController {
+	
+	@RequestMapping(value="/content1")
+	public String content1() {
+		return "tiles/content1";		//tiles설정.xml의 definition name="tiles/*"과 매칭
+	}
+	
+	@RequestMapping(value="/content2")
+	public String content2() {
+		return "tiles/content2";
+	}
+	
+	@RequestMapping(value="/content3")
+	public String content3() {
+		return "tiles/content3";
+	}
+
+}
